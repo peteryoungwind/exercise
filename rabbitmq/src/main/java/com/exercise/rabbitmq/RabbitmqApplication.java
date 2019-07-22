1c54b1cc76a238261e4424fbc629aec0addfb954
zhiqiang.pan <zhiqiang.pan@fanli.com>
2019-07-22 16:08:49 +0800

    搭建rabbitmq工程

---
+++ rabbitmq/src/main/java/com/exercise/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,15 @@
+package com.exercise.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhiqiang.pan
+ * @Date: 2019/7/22 16:05
+ */
+@SpringBootApplication
+public class RabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
+}
